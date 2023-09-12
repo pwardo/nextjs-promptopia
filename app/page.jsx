389ec4cecd1f88a70528b64f2389a87fe2704283
Patrick Ward <patwardo@gmail.com>
2023-09-12 12:41:20 +0100

    Add the title and desription to our home page

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,14 @@
 function Home() {
   return (
-    <div>Home</div>
+    <section className="w-full flex-center flex-col">
+      <h1 className="head_text text-center">Discover and Share
+        <br className="max-md:hidden" />
+        <span className="orange_gradient text-center"> AI-Powered Prompts</span>
+      </h1>
+      <p className="desc text-center">
+        Promptopia is an open source AI prompting tool for everyone to discover, create and share creative AI prompts
+      </p>
+    </section>
   )
 }
 
