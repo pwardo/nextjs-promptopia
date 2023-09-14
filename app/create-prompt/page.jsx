3e443753a7a34f7c58b3c27c3aec4234e99506bf
Patrick Ward <patwardo@gmail.com>
2023-09-14 16:52:04 +0100

    Save Prompt to database

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -2,10 +2,13 @@
 
 import { useState } from 'react';
 import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/navigation';
 import Form from '@components/Form'
 
 const CreatePrompt = () => {
+  const router = useRouter();
+  const { data: session } = useSession();
+
   const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
     prompt: '',
@@ -13,7 +16,27 @@ const CreatePrompt = () => {
   })
 
   const CreatePrompt = async (e) => {
+    e.preventDefault();
+    setSubmitting(true);
 
+    try {
+      const reponse = await fetch('/api/prompt/new', {
+        method: 'POST',
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag
+        }),
+      });
+
+      if (reponse.ok) {
+        router.push('/');
+      }
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setSubmitting(false);
+    }
   }
 
   return (
