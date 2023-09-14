1f0f9c15cd271ebc531cb75890636b563dacd6b4
Patrick Ward <patwardo@gmail.com>
2023-09-14 22:24:26 +0100

    Setup api request to get prompts for specific user

---
+++ app/profile/page.jsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+
+import Profile from '@components/profile';
+
+const MyProfile = () => {
+
+  const { data: session } = useSession();
+  const [prompts, setPrompts] = useState([]);
+
+  useEffect(() => {
+    const fetchPrompts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      console.log(data);
+      setPrompts(data);
+    };
+
+    if(session?.user.id) fetchPrompts();
+  }, []);
+
+
+  const handleEdit = () => {
+
+  }
+
+  const handleDelete = async () => {
+
+  }
+
+  return (
+    <Profile 
+      name='My'
+      desc='Welcome to my profile'
+      data={prompts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  )
+}
+
+export default MyProfile
